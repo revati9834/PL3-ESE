68d2debdc94bd8926b1cec53053d6bcf775b1c7e
Revati Jadhav <71699081+revati9834@users.noreply.github.com>
2021-01-01 17:56:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function bigImg(x) {
+  x.style.height = "300px";
+  x.style.width = "300px";
+}
+
+function normalImg(x) {
+  x.style.height = "230px";
+  x.style.width = "230px";
+}
+function fun() 
+		{  
+		alert("Welcome to Home-Mobile Review");  
+		}
+function fun1() 
+		{  
+		alert("Welcome to AboutUs");  
+		}
